c19d011b5d38bc67013732fcb8c9094f32c5c392
Mr-Yadav03 <vikritji@gmail.com>
2023-03-01 14:20:06 +0530

    This is first commit

---
+++ datatypes.py
@@ -0,0 +1,8 @@
+print("Testing multiline comment")
+"""
+This is multi line comment of python
+"""
+first_name="Micky"
+last_name="Yadav"
+age= 20
+print(f"my name is",first_name,last_name,"and my age is", age)
---
+++ variables.py
@@ -0,0 +1,29 @@
+#simle print function
+print("Hello Pyhton Micky \n")
+
+#class suffix vairbale
+#class_ = "class_ output with underscore suffix \n"
+#print(class_)
+
+class test:
+
+    #private or prefix variable
+    _var = "prefix under score variable which is supposed to be in module and private \n"
+    #print(_var)
+
+    __pri = "This is meant to be the private class or variable"
+    #print(__pri)
+
+    #class suffix vairbale
+    class_ = "class_ output with underscore suffix \n"
+    #print(class_)
+
+#print(__pri," -> This is outside of the class")
+#print(class_," -> testing ...")
+name = "Micky"
+#print(name.title())
+sur_name = "Yadav"
+roll_no = 1117
+print(f"My name is {name} {sur_name} and roll numbe is {roll_no}")
+
+print(type(roll_no))
